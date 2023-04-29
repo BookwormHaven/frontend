72a84d3f0c0ec23e46d1d3eb9cbc5552e12a9f9a
Jin Xuan Ow Yong <owyongjinxuan@gmail.com>
2023-04-29 22:06:01 +0800

    add book delete

--- app/controllers/books/view.js
+++ app/controllers/books/view.js
@@ -1,7 +1,20 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class BooksViewController extends Controller {
   queryParams = ['id'];
   @tracked id = null;
+
+  @action editBook() {
+    console.log('edit');
+  }
+
+  @action deleteBook() {
+    const { data } = this.model;
+    const { title } = data;
+    const res = confirm(`Are you sure you want to delete the book "${title}"?`);
+    if (!res) return;
+    this.model.deleteBook();
+  }
 }
--- app/routes/books/view.js
+++ app/routes/books/view.js
@@ -1,16 +1,40 @@
 import Route from '@ember/routing/route';
 import ENV from 'frontend/config/environment';
 import axios from 'axios';
+import { service } from '@ember/service';
 
 export default class BooksViewRoute extends Route {
+  @service router;
+
   async model(params) {
     const { API_BASE_URL } = ENV.APP;
     const { id } = params;
     const url = new URL(`${API_BASE_URL}/books/${id}`);
     url.searchParams.append('filter[include]', 'author');
-    return await axios
+    const data = await axios
       .get(url.toString())
       .then((res) => res.data)
       .catch(() => null);
+
+    return {
+      data,
+      deleteBook: this.deleteBook.bind(this),
+    };
+  }
+
+  deleteBook() {
+    const { id } = this.currentModel.data;
+    const { API_BASE_URL } = ENV.APP;
+    const url = new URL(`${API_BASE_URL}/books/${id}`);
+
+    axios
+      .delete(url)
+      .then(() => {
+        alert('Book has been successfully deleted.');
+        this.router.transitionTo('index');
+      })
+      .catch(() => {
+        alert('An error occured when deleting the book.');
+      });
   }
 }
